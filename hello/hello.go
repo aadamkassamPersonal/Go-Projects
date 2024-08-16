4caaa8c1892d98ccac9194e75e60a7db8a841acb
Aadam <aadamkassam@gmail.com>
2024-08-16 00:33:24 -0700

    altered name string for hello program

--- hello/hello.go
+++ hello/hello.go
@@ -15,7 +15,7 @@ func main() {
 	log.SetFlags(0)
 
 	// A slice of names.
-	names := []string{"Gladys", "Samantha", "Darrin", "Jared"}
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
 	// Request greeting messages for the names.
 	messages, err := greetings.Hellos(names)
